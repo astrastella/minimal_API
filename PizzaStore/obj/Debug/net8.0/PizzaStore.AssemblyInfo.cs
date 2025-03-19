5a4313fa5153fb11975cbcbbaf4a8fa7046371e5
astrastella <30526463+astrastella@users.noreply.github.com>
2025-03-19 11:31:48 +0000

    refactor: remove /bin and /obj

--- PizzaStore/obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
+++ PizzaStore/obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d39a10a32fb91e91d8959d5d6eeee2ef32c3096b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c74f0d0151d87335aebfbf65a36bd6ed7cf333fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
